29456c22f8475bd22c06d16c2639b7a259cffea0
lpalanti <lucaspalanti@hotmail.com>
2025-04-24 15:14:25 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
