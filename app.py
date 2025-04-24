c987d31cc0fcb052d62c018d85bda8af8495c35e
lpalanti <lucaspalanti@hotmail.com>
2025-04-24 16:00:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,13 +31,15 @@ elif idade >= 12:
 else:
     st.write("Criança")
 
-st.title("Números Pares de 1 a 20")
+st.title("Números Pares e Ímpares de 1 a 20")
 
 pares = [n for n in range(1, 21) if n % 2 == 0]
 impares = [n for n in range(1, 21) if n % 2 != 0]
 
+st.subheader("Números Pares:")
 for numero in pares:
     st.write(numero)
 
+st.subheader("Números Ímpares:")
 for numero in impares:
     st.write(numero)
